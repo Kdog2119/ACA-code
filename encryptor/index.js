7d24d60ee24a4f085c153cbd0edb55293d2c49cd
Kdog2119 <128638582+Kdog2119@users.noreply.github.com>
2023-05-10 13:00:57 -0700

    Update index.js

--- encryptor/index.js
+++ encryptor/index.js
@@ -20,4 +20,25 @@ function encrypt(text) {
     }
     
     return result;
-}// encrypt
+
+}
+
+function decrypt(text) {
+    let result = '';
+
+    for (let i = 0; i < text.length; i++) {
+      let letter = text[i];
+      let letterIndex = encrypt_key.indexOf(letter);
+    if (letterIndex < 0) {
+       
+      result += letter;  
+    }
+        
+      else {
+        result += letters[letterIndex]
+      }
+    }
+    
+    return result;
+
+}
